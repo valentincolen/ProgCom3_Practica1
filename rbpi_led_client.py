cee1b7f703b51e90abb2018d05e56609129525b0
Javier <valentincolen@outlook.com>
2018-03-27 23:23:55 +0200

    Added advance logging and fix some bugs

--- rbpi_led_client.py
+++ rbpi_led_client.py
@@ -1,92 +1,126 @@
 import argparse
+import logging
 import socket
 
+# Creation of an ArgumentParser object with description of the program.
 parser = argparse.ArgumentParser(
-    description='Client to connect to the server ')  # Creation of an ArgumentParser object with description of the program.
+    description='Client to connect to an UDP or TCP server, it also collect execution data into a log file with advanced logging')
 
 parser.add_argument('--ipserver', '-i', help='Ip of the server where you connect', type=str)
 parser.add_argument('--message', '-m',
-                    help='Request action to the server. Actions available: switch_on, switch_off, status', type=str)
-parser.add_argument('--type', '-t', help='Type of client. Available: TCP, UDP', type=str)
+                    help='Request action to the server', type=str,
+                    choices=['switch_on', 'switch_off', 'status'])
+parser.add_argument('--type', '-t', help='Type of client', type=str, choices=['TCP', 'UDP'])
+parser.add_argument('--path', '-pt', help='Specify the path where the log is saved', default='', type=str)
+parser.add_argument('--file_name', '-f', help='Name of the log file', default='client', type=str)
 
 
+# Class that include the commons between TCP and UDP
 class Client:
-    def __init__(self, protocol, server_ip):
+    def __init__(self, server_ip, filename, logpath, protocol):
+        # Advanced Logging
+        #   Define format
+        logformatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+        self.rootLogger = logging.getLogger()
+        self.rootLogger.setLevel(logging.DEBUG)
+        #   Handler to send messages to disk files
+        filehandler = logging.FileHandler("{0}/{1}.log".format(logpath, filename))
+        filehandler.setFormatter(logformatter)
+        filehandler.setLevel(logging.DEBUG)
+        self.rootLogger.addHandler(filehandler)
+        #   Handler to send messages to streams like sys.stdout, sys.stderr or any file-like object
+        consolehandler = logging.StreamHandler()
+        consolehandler.setFormatter(logformatter)
+        consolehandler.setLevel(logging.INFO)
+        self.rootLogger.addHandler(consolehandler)
+
         self.sock = socket.socket(socket.AF_INET, protocol)
         self.server_address = (server_ip, 10000)
-        print 'Socket created'
+        self.rootLogger.debug('Socket created')
         self.buffer = 4096
 
     def close_client(self):
         self.sock.close()
-        print 'Socket closed'
+        self.rootLogger.debug('Socket closed')
+
+    #Function to add advanced logging INFO mesages
+    def logg_info(self, msg):
+        self.rootLogger.info(msg)
+
+    # Function to add advanced logging DEBUG mesages
+    def logg_debug(self, msg):
+        self.rootLogger.debug(msg)
 
 
+# Class with inheritance from Client class
 class ClientUDP(Client):
-    def __init__(self, server_ip):
-        Client.__init__(self, socket.SOCK_DGRAM, server_ip)
+    def __init__(self, server_ip, filename, logpath):
+        # Inherit from the Client class the init function
+        Client.__init__(self, server_ip, filename, logpath, protocol=socket.SOCK_DGRAM)
 
+    # Send data through UDP protocol
     def send_data(self, message):
+        self.logg_debug('Sending data')
         self.sock.sendto(message, self.server_address)
-        print 'Data sent'
+        self.logg_debug('Data sent')
 
+    # Receive data through UDP protocol
     def receive_data(self):
-        print 'Waiting for data'
+        self.logg_debug('Waiting for data')
         received_data, server_ip = self.sock.recvfrom(4096)
-        print 'Data received'
+        self.logg_debug('Data received')
         return [received_data, server_ip]
 
 
 class ClientTCP(Client):
-    def __init__(self, server_ip):
-        Client.__init__(self, socket.SOCK_STREAM, server_ip)
+    def __init__(self, server_ip, filename, logpath):
+        Client.__init__(self, server_ip, filename, logpath, protocol=socket.SOCK_STREAM)
 
     def connection(self):
         self.sock.connect(self.server_address)
-        print 'Connection done'
+        self.logg_debug('Connection done')
 
     def send_data_connected(self, msg_to_send):
         self.sock.sendall(msg_to_send)
-        print 'Data sent'
+        self.logg_debug('Data sent')
 
     def receive_data_connected(self):
-        print 'Waiting for data'
-        return self.sock.recv(self.buffer)
+        self.logg_debug('Waiting for data')
+        new_data = self.sock.recv(self.buffer)
+        self.logg_debug('New data received')
+        return new_data
 
 
-def treat_action():
-    msg_to_send = args.message
-    while msg_to_send != ('swittch_on' or 'switch_off' or 'status'):
-        if msg_to_send == 'switch_on':
-            return 'ON'
-        elif msg_to_send == 'switch_off':
-            return 'OFF'
-        elif msg_to_send == 'status':
-            return 'STAT'
-        else:
-            msg_to_send = raw_input('Wrong action. Actions available:\nswitch_on\nswitch_off\nstatus\n')
+def treat_action(msg_to_send):
+    if msg_to_send == 'switch_on':
+        return 'ON'
+    elif msg_to_send == 'switch_off':
+        return 'OFF'
+    elif msg_to_send == 'status':
+        return 'STAT'
 
 
 def cliudp():
-    client = ClientUDP(args.ipserver)
+    client = ClientUDP(args.ipserver, args.file_name, args.path)
     try:
-        msg_to_send = treat_action()
+        msg_to_send = treat_action(args.message)
+        client.logg_debug(msg_to_send)
         client.send_data(msg_to_send)
         [data, server] = client.receive_data()
         if data:
-            print data
+            client.logg_info(data)
     finally:
         client.close_client()
 
 
 def clitcp():
-    client = ClientTCP(args.ipserver)
+    client = ClientTCP(args.ipserver, args.file_name, args.path)
     try:
         client.connection()
-        msg_to_send = treat_action()
+        msg_to_send = treat_action(args.message)
         client.send_data_connected(msg_to_send)
         msg_received = client.receive_data_connected()
-        print msg_received
+        client.logg_info(msg_received)
 
     finally:
         client.close_client()
@@ -98,5 +132,3 @@ if __name__ == "__main__":
         clitcp()
     elif args.type == 'UDP':
         cliudp()
-    else:
-        print'Wrong type. Available:\nTCP\nUDP\n'
--- rbpi_led_server.py
+++ rbpi_led_server.py
@@ -1,71 +1,113 @@
 import argparse
+import logging
 import socket
 
 import RPi.GPIO as GPIO
 
-parser = argparse.ArgumentParser(description='Program that create an UDP or TCP Server ')
+# Creation of an ArgumentParser object with description of the program.
+parser = argparse.ArgumentParser(
+    description='Program that create an UDP or TCP Server, it it also collect execution data into a log file with advanced logging')
 parser.add_argument('--pin', '-p', help='Pin where is connected the led', type=int)
-parser.add_argument('--ip', help='', default='0.0.0.0', type=str)
-parser.add_argument('--type', '-t', help='Type of server. Available: TCP, UDP', type=str)
+# With IP 0.0.0.0 the server can listen to any IP from their interfaces.
+parser.add_argument('--ip', help='IP of the server', default='0.0.0.0', type=str)
+parser.add_argument('--type', '-t', help='Type of server', type=str, choices=['TCP', 'UDP'])
+parser.add_argument('--path', '-pt', help='Specify the path where the log is saved', default='', type=str)
+parser.add_argument('--file_name', '-f', help='Name of the log file', default='server', type=str)
 
 
+# Class that include the commons between TCP and UDP
 class Server:
-    def __init__(self, pin, ip, protocol):
+    def __init__(self, pin, ip, log_path, file_name, protocol):
+        # Advanced Logging
+        #   Define format
+        logformatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+        self.rootLogger = logging.getLogger()
+        self.rootLogger.setLevel(logging.DEBUG)
+        #   Handler to send messages to disk files
+        filehandler = logging.FileHandler("{0}/{1}.log".format(log_path, file_name))
+        filehandler.setFormatter(logformatter)
+        filehandler.setLevel(logging.DEBUG)
+        self.rootLogger.addHandler(filehandler)
+        #   Handler to send messages to streams like sys.stdout, sys.stderr or any file-like object
+        consolehandler = logging.StreamHandler()
+        consolehandler.setFormatter(logformatter)
+        consolehandler.setLevel(logging.INFO)
+        self.rootLogger.addHandler(consolehandler)
+
         self.pin = pin
         self.server_address = (ip, 10000)
+        # Socket creation
         self.sock = socket.socket(socket.AF_INET, protocol)
+        self.rootLogger.debug('Socket created')
+        # Socket binding
         self.sock.bind(self.server_address)
-        print '\nSocket bound'
+        self.rootLogger.debug('Socket bound')
 
     def close_server(self):
+        # Socket close
         self.sock.close()
-        print 'Socket closed'
+        self.rootLogger.debug('Socket closed')
 
 
+# Class with inheritance from Server class
 class ServerUDP(Server):
-    def __init__(self, pin, ip):
-        Server.__init__(self, pin, ip, socket.SOCK_DGRAM)
+    def __init__(self, pin, ip, log_path, file_name):
+        # Inherit from the Server class the init function
+        Server.__init__(self, pin, ip, log_path, file_name, protocol=socket.SOCK_DGRAM)
 
+    # Receive data through UDP protocol.
     def receive_data(self):
-        print '\nWaiting for data...'
+        self.rootLogger.info('Waiting for data...')
         self.data, self.client_address = self.sock.recvfrom(4096)
+        self.rootLogger.info('New data received from ' + self.client_address[0])
         return self.data
 
-    def send_data(self, information_to_send, destinatary):
-        print 'Sending to ' + self.client_address[0]
-        self.sock.sendto(information_to_send, destinatary)
-        print 'Data sent'
+    # Send data through UDP protocol.
+    def send_data(self, information_to_send, receiver):
+        self.rootLogger.info('Sending to ' + self.client_address[0])
+        self.sock.sendto(information_to_send, receiver)
+        self.rootLogger.debug('Data sent')
 
 
+# Class with inheritance from Server class
 class ServerTCP(Server):
-    def __init__(self, pin, ip):
-        Server.__init__(self, pin, ip, socket.SOCK_STREAM)
-        self.sock.listen(1)  # This will start the server to 'listen' for new connections
+    def __init__(self, pin, ip, log_path, file_name):
+        Server.__init__(self, pin, ip, log_path, file_name, protocol=socket.SOCK_STREAM)
+        # Listen for incoming connections
+        self.sock.listen(1)
+        self.rootLogger.debug('Listening...')
 
-    def accept_conections(self):
+    # Wait for a connection.
+    def accept_connections(self):
+        self.rootLogger.info('Waiting for a new connection')
         self.connection, self.client_address = self.sock.accept()
+        self.rootLogger.debug('Connected to ' + self.client_address[0])
 
-
+    # Receive data through TCP protocol.
     def receive_data_connected(self):
         data = self.connection.recv(4096)
         return data
-        
+
+    # Send data through TCP protocol.
     def send_data_connected(self, message):
+        self.rootLogger.info('Sending to ' + self.client_address[0])
         self.connection.sendall(message)
 
+    # Close the connection.
     def close_connection(self):
         self.connection.close()
+        self.rootLogger.debug('Connection with ' + self.client_address[0] + ' closed')
 
 
+# Check the state of the pin selected.
 def check_status(pin):
     return GPIO.input(pin)
 
 
+# Check actual pin state to perform the right action
 def get_led_status(pin):
-    print 'Checking status'
     if check_status(pin):
         return 'LED is ON'
-
     else:
         return 'LED is OFF'
 
@@ -86,6 +128,7 @@ def switch_off(pin):
         return ''
 
 
+# Treat information with available orders in order to send smaller packets
 def check_order(data_to_treat, pin):
     if data_to_treat == 'STAT':
         return get_led_status(pin)
@@ -95,18 +138,19 @@ def check_order(data_to_treat, pin):
         return switch_off(pin)
 
 
+# Configure the mode of the pin and set it up as a output.
 def pin_conf(pin):
     GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     GPIO.setup(pin, GPIO.OUT)
 
 
 def servudp():
-    servu = ServerUDP(args.pin, args.ip)
-    pin_conf(servu.pin)
+    servu = ServerUDP(args.pin, args.ip, args.path, args.file_name)  # Create an instance of the ServerUDP class
+    pin_conf(servu.pin)  # Configure the pin
     try:
-        while True:
+        while True:  # Call the different functions to receive data and do the action asked.
             data_to_treat = servu.receive_data()
-            print 'New data received\n'
             msg_to_send = check_order(data_to_treat, servu.pin)
             servu.send_data(msg_to_send, servu.client_address)
     finally:
@@ -114,26 +158,22 @@ def servudp():
 
 
 def servtcp():
-    servt = ServerTCP(args.pin, args.ip)
+    servt = ServerTCP(args.pin, args.ip, args.path, args.file_name)
     pin_conf(servt.pin)
     while True:
-        print 'Waiting for a new connection'
-        servt.accept_conections()
-        print 'Connection accepted'
+        servt.accept_connections()
         try:
             msg_received = servt.receive_data_connected()
             msg_to_send = check_order(msg_received, servt.pin)
             servt.send_data_connected(msg_to_send)
         finally:
             servt.close_connection()
-            print 'Connection closed'
 
 
 if __name__ == "__main__":
+    # Create an instance from the parser library.
     args = parser.parse_args()
     if args.type == 'TCP':
         servtcp()
     elif args.type == 'UDP':
         servudp()
-    else:
-        print 'Wrong type. Available:\nTCP\nUDP\n'
